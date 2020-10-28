7fbc4cb916bc6daed4e6d47330b44fcbbcaf564a
prawsang <prawsang.c@gmail.com>
2020-10-28 20:01:53 +0700

    back: minor fixes

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -89,11 +89,11 @@ router.put("/edit", [authenticateToken], async (req, res) => {
 });
 
 // Get posts of a user
-router.get("/posts/:userId", async (req, res) => {
+router.get("/posts/:username", async (req, res) => {
   try {
     await mongoose.connection.db
       .collection("posts")
-      .find({ userId: req.params.userId })
+      .find({ username: req.params.username })
       .toArray()
       .then((posts) => {
         res.status(200).json({ posts });
